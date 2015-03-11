6e16e1164568789b3db7f82280a1ff69c9c01d06
Sam Harwell <sam@tunnelvisionlabs.com>
2015-03-11 07:18:47 -0500

    Use the same strong name key for the net35 build as the others

--- build/keys.ps1
+++ build/keys.ps1
@@ -4,7 +4,7 @@ If ($Version.Contains('-')) {
 
 	# Use the development keys
 	$Keys = @{
-		'net35-client' = 'ff1f43501ca6fae4'
+		'net35-client' = '865cbfcb04071065'
 		'net40-client' = '865cbfcb04071065'
 		'portable-net40' = '865cbfcb04071065'
 		'portable-net45' = '865cbfcb04071065'
@@ -14,7 +14,7 @@ If ($Version.Contains('-')) {
 
 	# Use the final release keys
 	$Keys = @{
-		'net35-client' = '2d5107316a49ad7c'
+		'net35-client' = '8acff7051142f28d'
 		'net40-client' = '8acff7051142f28d'
 		'portable-net40' = '8acff7051142f28d'
 		'portable-net45' = '8acff7051142f28d'
