2c37b41cb470cba31a42626e42d0970505c08df5
Sam Harwell <sam@tunnelvisionlabs.com>
2015-05-13 12:47:02 -0500

    Make sure to run tests using the correct target framework

--- System.Collections.Immutable/tests/DebuggerAttributes.cs
+++ System.Collections.Immutable/tests/DebuggerAttributes.cs
@@ -17,25 +17,48 @@ namespace System.Diagnostics
         internal static void ValidateDebuggerTypeProxyProperties(object obj)
         {
             // Get the DebuggerTypeProxyAttibute for obj
+#if NET45PLUS
             var attrs = 
                 obj.GetType().GetTypeInfo().CustomAttributes
                 .Where(a => a.AttributeType == typeof(DebuggerTypeProxyAttribute))
                 .ToArray();
+#elif NET40PLUS
+            var attrs = 
+                obj.GetType().GetCustomAttributesData()
+                .Where(a => a.Constructor.DeclaringType == typeof(DebuggerTypeProxyAttribute))
+                .ToArray();
+#else
+            var attrs = obj.GetType().GetCustomAttributes(typeof(DebuggerTypeProxyAttribute), false);
+#endif
             if (attrs.Length != 1)
             {
                 throw new InvalidOperationException(
                     string.Format("Expected one DebuggerTypeProxyAttribute on {0}.", obj));
             }
+
+#if NET40PLUS
             var cad = (CustomAttributeData)attrs[0];
+#else
+            var cad = (DebuggerTypeProxyAttribute)attrs[0];
+#endif
 
             // Get the proxy type.  As written, this only works if the proxy and the target type
             // have the same generic parameters, e.g. Dictionary<TKey,TValue> and Proxy<TKey,TValue>.
             // It will not work with, for example, Dictionary<TKey,TValue>.Keys and Proxy<TKey>,
             // as the former has two generic parameters and the latter only one.
+#if NET40PLUS
             Type proxyType = cad.ConstructorArguments[0].ArgumentType == typeof(Type) ?
                 (Type)cad.ConstructorArguments[0].Value :
                 Type.GetType((string)cad.ConstructorArguments[0].Value);
+#else
+            Type proxyType = cad.Target != null ? cad.Target : Type.GetType(cad.ProxyTypeName);
+#endif
+
+#if NET45PLUS
             var genericArguments = obj.GetType().GenericTypeArguments;
+#else
+            var genericArguments = obj.GetType().GetGenericArguments();
+#endif
             if (genericArguments.Length > 0)
             {
                 proxyType = proxyType.MakeGenericType(genericArguments);
@@ -58,19 +81,33 @@ namespace System.Diagnostics
         internal static void ValidateDebuggerDisplayReferences(object obj)
         {
             // Get the DebuggerDisplayAttribute for obj
+#if NET45PLUS
             var attrs = 
                 obj.GetType().GetTypeInfo().CustomAttributes
                 .Where(a => a.AttributeType == typeof(DebuggerDisplayAttribute))
                 .ToArray();
+#elif NET40PLUS
+            var attrs = 
+                obj.GetType().GetCustomAttributesData()
+                .Where(a => a.Constructor.DeclaringType == typeof(DebuggerDisplayAttribute))
+                .ToArray();
+#else
+            var attrs = obj.GetType().GetCustomAttributes(typeof(DebuggerDisplayAttribute), false);
+#endif
             if (attrs.Length != 1)
             {
                 throw new InvalidOperationException(
                     string.Format("Expected one DebuggerDisplayAttribute on {0}.", obj));
             }
+
+#if NET40PLUS
             var cad = (CustomAttributeData)attrs[0];
 
             // Get the text of the DebuggerDisplayAttribute
             string attrText = (string)cad.ConstructorArguments[0].Value;
+#else
+            string attrText = ((DebuggerDisplayAttribute)attrs[0]).Value;
+#endif
 
             // Parse the text for all expressions
             var references = new List<string>();
---
+++ System.Collections.Immutable/tests/Tuple`3.cs
@@ -0,0 +1,44 @@
+﻿#if !NET40PLUS
+
+namespace System
+{
+    public class Tuple<T1, T2, T3>
+    {
+        private readonly T1 _item1;
+        private readonly T2 _item2;
+        private readonly T3 _item3;
+
+        public Tuple(T1 item1, T2 item2, T3 item3)
+        {
+            _item1 = item1;
+            _item2 = item2;
+            _item3 = item3;
+        }
+
+        public T1 Item1
+        {
+            get
+            {
+                return _item1;
+            }
+        }
+
+        public T2 Item2
+        {
+            get
+            {
+                return _item2;
+            }
+        }
+
+        public T3 Item3
+        {
+            get
+            {
+                return _item3;
+            }
+        }
+    }
+}
+
+#endif
